9ea5278e714e68b4d78bcddfb85fb3d55c681e3e
NevedomaZverushka <lunna.savovich@gmail.com>
2020-01-20 10:15:42 +0200

    Added header with dynamic elements

--- beep-boop/src/App.js
+++ beep-boop/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style/App.scss';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header'
+import Content from './components/Content'
+
+const initState = {
+  currState: true
+}
+const reducer = (state = initState) => {
+  return state
+}
+const store = createStore(reducer);
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Header />
+        <Content />
+      </Provider>
+    );
+  }
 }
 
 export default App;
